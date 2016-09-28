bbdd4eefa89e55597a86c9ceb11a9ee3777df309
OmarHassanCU <S199803405@gmail.com>
2016-09-27 22:34:55 -0400

    Add files via upload

---
+++ 101036344-a1q1.py
@@ -0,0 +1,32 @@
+#Name: Omar Hassan
+#Student Number: 101036344
+#Assignment 1.1
+#Gaddis, T. (2015). "Starting Out With Python"
+#CULearn.(2016-09-23).Retrieved from https://culearn.carleton.ca/moodle/pluginfile.php/1682280/mod_resource/content/1/COMP1405A-F16-XXX-XX-%28Plagiarism%20Policy%29.pdf
+from SimpleGraphics import * #Using SimpleGraphic Library 
+
+#Changing the background colour to white 
+setColor(255,255,255)
+rect(0,0,99,49)
+
+#Draw a Black Polygon 
+setColor(0,0,0)
+polygon(10,36,40,37,40,49,33,49,33,45,18,45,17,49,9,49)
+polygon(72,0,81,0,81,11,82,11,82,18,83,18,83,32,84,32,84,36,85,36,85,0,93,0,92,49,77,49,72,0)
+
+
+#Draw a Purple Polygon
+setColor(82,0,135)
+polygon(81,0,85,0,85,11,84,12,83,12,82,12,82,11,81,11)
+
+#Draw a Gray Polygon
+setColor(83,83,83)
+polygon(82,13,84,13,84,12,85,12,85,33,84,33,84,32,83,32,83,18,82,18)
+
+#Draw a Purple Rectangle 
+setColor(82,0,135)
+rect(84,33,1,3)         
+
+#Draw a Blue Rectangle
+setColor(0,0,135)          
+rect(17,45,16,5)
